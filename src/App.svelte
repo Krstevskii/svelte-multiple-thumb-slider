f146c9b188c43a7120c287008d1ef430a6ca8a80
Ivan Krstevski <Ivan.Krstevski@netcetera.com>
2019-10-09 10:57:22 +0200

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+  import ImprovedSlider from "./ImprovedSlider.svelte";
+
+  const numberOfColumns = 4;
+</script>
+
+<style>
+</style>
+
+<div class="outerWidth">
+  <ImprovedSlider
+    columnWidthInPercent={[25, 25, 25, 25]}
+    on:columnWidthChange={e => console.log(e.detail.columnsWidths)} />
+</div>
